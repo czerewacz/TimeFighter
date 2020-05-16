1c594dfb33f9caf247bafe69ad36fecc86c3471b
Marcelo Czerewacz <czerewacz@gmail.com>
2020-05-16 09:37:07 +0200

    Added Game Score feature

--- app/src/main/java/com/raywenderlich/timefighter/GameActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/GameActivity.kt
@@ -2,11 +2,51 @@ package com.raywenderlich.timefighter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
+
+    private lateinit var gameScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+    private lateinit var tapMeButton: Button
+    private var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        // 1
+        gameScoreTextView = findViewById(R.id.game_score_text_view)
+
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+
+        tapMeButton = findViewById(R.id.tap_me_button)
+
+// 2
+        tapMeButton.setOnClickListener { incrementScore() }
+
+        // connect views to variables
     }
+
+    private fun incrementScore() {
+        // increment score logic
+            score++
+
+            val newScore = "Your Score: $score"
+            gameScoreTextView.text = newScore
+    }
+
+    private fun resetGame() {
+        // reset game logic
+    }
+
+    private fun startGame() {
+        // start game logic
+    }
+
+    private fun endGame() {
+        // end game logic
+    }
+    
 }
